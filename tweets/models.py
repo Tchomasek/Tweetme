4a135cd09e8f802ca123207de0283c767e61a90e
Tom <tchom.asek@centrum.cz>
2020-11-29 14:19:08 +0100

    -

--- tweets/models.py
+++ tweets/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 class Tweet(models.Model):
     
-    content = models.TextField()
-    image = models.FileField(upload_to='images/')
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
+    
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+from .models import Tweet
 
-# Create your views here.
+def home_view(request, *args, **kwargs):
+    return HttpResponse('<h1>hello world</h1>')
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    data = {
+        'id':tweet_id,
+    }
+    status=200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = 'not found'
+        status = 404
+    
+    return JsonResponse(data, status = status)
