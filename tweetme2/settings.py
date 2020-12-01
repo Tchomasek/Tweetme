c116fe479728c4c720bce9b3efc133a5b18ff88c
Tom <tchom.asek@centrum.cz>
2020-12-01 11:38:25 +0100

    before reset

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e%5s%u5)&c@estq#0#n-ktey-mnqu^x)fl)mg-(bs(+*6u8vx8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'tweetme2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,36 @@
+import random
+from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
+from .forms import TweetForm
 from .models import Tweet
 
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+
 def home_view(request, *args, **kwargs):
-    return HttpResponse('<h1>hello world</h1>')
+    return render(request, 'pages/home.html', context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None  
+    print('next url', next_url)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
+            return redirect(next_url)
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})    
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{'id':x.id, 'content': x.content, 'likes': random.randint(0,100)}for x in qs]
+    data = {
+        'isUser': False,
+        'response': tweets_list
+    }
+    return JsonResponse(data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     data = {
